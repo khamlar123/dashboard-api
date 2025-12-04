217996d5c80159827e9e0efe85aa8cc5c80f4237
khamlar123 <khamlar_chanthavong@yahoo.com>
2025-12-04 09:05:14 +0700

    upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,11 +47,11 @@ import { DashboardModule } from './modules/dashboard/dashboard.module';
     }),
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as 'mariadb',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'test',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false, // Sync models with database (disable in production)
     }),
